5b18ba36ae9ad1c6dbd9849d1318eb084f291b93
Frieren <ltpk2182006@gmail.com>
2026-03-02 12:24:36 +0700

    Add images and define the game variables

---
+++ ChromeDinosaur.java
@@ -0,0 +1,71 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class ChromeDinosaur extends JPanel  {
+    int boardWidth = 750;
+    int boardHeight = 250;
+
+    //images
+    Image dinosaurImg;
+    Image dinosaurDeadImg;
+    Image dinosaurJumpImg;
+    Image cactus1Img;
+    Image cactus2Img;
+    Image cactus3Img;
+
+    class Block {
+        int x;
+        int y;
+        int width;
+        int height;
+        Image img;
+
+        Block(int x, int y, int width, int height, Image img) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.img = img;
+        }
+    }
+
+    //dinosaur
+    int dinosaurWidth = 88;
+    int dinosaurHeight = 94;
+    int dinosaurX = 50;
+    int dinosaurY = boardHeight - dinosaurHeight;
+
+    Block dinosaur;
+
+
+
+     public ChromeDinosaur() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.lightGray);
+        setFocusable(true);
+        
+        dinosaurImg = new ImageIcon(getClass().getResource("./img/dino-run.gif")).getImage();
+        dinosaurDeadImg = new ImageIcon(getClass().getResource("./img/dino-dead.png")).getImage();
+        dinosaurJumpImg = new ImageIcon(getClass().getResource("./img/dino-jump.png")).getImage();
+        cactus1Img = new ImageIcon(getClass().getResource("./img/cactus1.png")).getImage();
+        cactus2Img = new ImageIcon(getClass().getResource("./img/cactus2.png")).getImage();
+        cactus3Img = new ImageIcon(getClass().getResource("./img/cactus3.png")).getImage();
+
+        //dinosaur
+        dinosaur = new Block(dinosaurX, dinosaurY, dinosaurWidth, dinosaurHeight, dinosaurImg);
+     }
+     
+     public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        //dinosaur
+        g.drawImage(dinosaur.img, dinosaur.x, dinosaur.y, dinosaur.width, dinosaur.height, null);
+    }
+}
+    
+   
